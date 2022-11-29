1c748db24861aada667665b35476c57f1a38403a
emilysupercode <sheil.emily.sc@gmail.com>
2022-11-29 20:45:33 +0100

    unstyled, functional income and expense tally

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
+import Buttons from "./components/buttons/Buttons";
 
 function App() {
+let [balance, setBalance] = useState(0);
+const [userInput, setUserInput] = useState("");
+const getInput =  event => {
+  setUserInput(event.target.value);  
+}
+
+function Income() {
+  let userInputNumber = Number(userInput);
+setBalance(balance + userInputNumber); 
+setUserInput("");
+}
+
+function Expense() {
+  let userInputNumber = Number(userInput)
+  setBalance(balance-userInputNumber)
+  setUserInput("");
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p classname="balanceP">{balance}</p>
+      <input type="number" id="amountChange" onChange={getInput} value={userInput}/>
+      <button type="button" onClick={Income}>Log Income</button>
+      <button type="button" onClick={Expense}>Log Expense</button>
     </div>
   );
 }
